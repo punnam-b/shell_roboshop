314d384f9b00d0c045a4a2858ffc03e9ac05e49a
awsdevopsonline <punnam.elite@gmail.com>
2026-02-06 23:03:33 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -7,23 +7,22 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for intance in $@
 do
-        INSTANCE_ID=$(aws ec2 run-instances \
+        INSTANCE_ID=$( aws ec2 run-instances \
         --image-id $AMI_ID \
         --instance-type t3.micro  \
         --security-group-ids $SG_ID \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
          --query 'Instances[0].InstanceId' \
-        --output text
-        )
+        --output text )
 
         if [ $instance == "frontend" ]; then
-        IP=$(aws ec2 describe-instances \
+        IP=$( aws ec2 describe-instances \
                 --instance-ids $INSTANCE_ID \
                 --query 'Reservations[].Instances[].PublicIpAddress' \
                 --output text
             )
         else        
-        IP=$(aws ec2 describe-instances \
+        IP=$( aws ec2 describe-instances \
                 --instance-ids $INSTANCE_ID \
                 --query 'Reservations[].Instances[].PrivateIpAddress' \
                 --output text
