dc46175957a0aa74c20926ba4032624add4d53fa
awsdevopsonline <punnam.elite@gmail.com>
2026-02-06 22:59:40 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -15,7 +15,7 @@ do
          --query 'Instances[0].InstanceId' \
         --output text)
 
-        if [ $instance == "frontend" ] then
+        if [ $instance == "frontend" ]; then
         IP=$(aws ec2 describe-instances \
                 --instance-ids $INSTANCE_ID \
                 --query 'Reservations[].Instances[].PublicIpAddress' \
@@ -27,7 +27,7 @@ do
                 --query 'Reservations[].Instances[].PrivateIpAddress' \
                 --output text
             )
-            fi
+        fi
 
 done
 
