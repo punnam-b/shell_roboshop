adea04777b474465c461ce3cec88fc68d78ff446
awsdevopsonline <punnam.elite@gmail.com>
2026-02-06 23:01:15 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -10,10 +10,11 @@ do
         INSTANCE_ID=$(aws ec2 run-instances \
         --image-id $AMI_ID \
         --instance-type t3.micro  \
-        --security-group-ids $SG_ID \       
+        --security-group-ids $SG_ID \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
          --query 'Instances[0].InstanceId' \
-        --output text)
+        --output text
+        )
 
         if [ $instance == "frontend" ]; then
         IP=$(aws ec2 describe-instances \
