ca49c8e4e181093e1487616ecfb5bad4881b1916
awsdevopsonline <punnam.elite@gmail.com>
2026-02-07 13:34:59 +0530

    shell-roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -75,7 +75,8 @@ systemctl enable catalogue  &>>LOGFILENAME
 systemctl start catalogue &>>LOGFILENAME
 VALIDATE $? "Enable & START Catalogue"
 
-echo " working directory '$PWD'"
+SAMPLE
+echo  working directory "$PWD"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>LOGFILENAME
 VALIDATE $? "Install Mongodb client"
