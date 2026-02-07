ac666a23a9a1d83702c553483c3d9d8bd20eb49b
awsdevopsonline <punnam.elite@gmail.com>
2026-02-07 15:09:31 +0530

    shell-roboshop

---
+++ rabbitmq.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGFOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGFOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MYSQL_HOST="mysql.dhruvanakshatra.in"
+SCRIPT_DIR=$PWD
+echo " working directory '$SCRIPT_DIR'"
+
+if [ $USERID -ne 0 ]; then
+    echo  -e "$R .pls run with root user $N"  | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGFOLDER
+
+VALIDATE (){
+if [ $1 -ne 0 ]; then
+    echo -e "$2....$R FAILED $N" | tee -a $LOGS_FILE
+    exit 1
+else
+    echo -e "$2  ....$G SUCCESS $N" | tee -a $LOGS_FILE
+fi
+
+}
+
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATE $? "coping repo mq server"
+
+
+dnf install rabbitmq-server -y
+VALIDATE $? "install rabbit mq server"
+
+systemctl enable rabbitmq-server
+systemctl start rabbitmq-server
+VALIDATE $? "enable and start rabbit mq server"
+
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+VALIDATE $? "created user and give permissions"
+
