5b7aabc35868ef8a1b961a5cd9db04863bf58b1c
awsdevopsonline <punnam.elite@gmail.com>
2026-02-07 11:57:41 +0530

    shell-roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -18,7 +18,7 @@ mkdir -p $LOGFOLDER
 
 VALIDATE (){
 if [ $1 -ne 0 ]; then
-    echo -e"$2....$R FAILED $N" | tee -a $LOGS_FILE
+    echo -e "$2....$R FAILED $N" | tee -a $LOGS_FILE
     exit 1
 else
     echo -e "$2  ....$G SUCCESS $N" | tee -a $LOGS_FILE
