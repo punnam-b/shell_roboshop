61e05a7f3324b0815651468c13e7ee8a17da0bd9
awsdevopsonline <punnam.elite@gmail.com>
2026-02-07 11:51:46 +0530

    shell-roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -1,1 +1,46 @@
 #!/bin/bash
+
+USERID=$(id -u)
+LOGFOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGFOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+if [ $USERID -ne 0 ]; then
+    echo  -e "$R .pls run with root user $N"  | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGFOLDER
+
+VALIDATE (){
+if [ $1 -ne 0 ]; then
+    echo -e"$2....$R FAILED $N" | tee -a $LOGS_FILE
+    exit 1
+else
+    echo -e "$2  ....$G SUCCESS $N" | tee -a $LOGS_FILE
+fi
+
+}
+
+dnf module disable nodejs -y &>>$LOGS_FILE
+VALIDATE $? " default  node js version disable"
+
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
+VALIDATE $?  "enable nodejs"
+
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Install NODE JS"
+
+#creating system user
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "create system user"
+
+mkdir /app 
+VALIDATE $? "create directory app"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+VALIDATE $? "Download catalogue code"
