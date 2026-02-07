15c21009e253304b36e165cb0e3fd95952bf31aa
awsdevopsonline <punnam.elite@gmail.com>
2026-02-07 14:52:48 +0530

    shell-roboshop

---
+++ shipping.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGFOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGFOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MYSQL_HOST="mysql.dhruvanakshatra.in"
+SCRIPT_DIR=$PWD
+echo " working directory '$SCRIPT_DIR'"
+
+if [ $USERID -ne 0 ]; then
+    echo  -e "$R .pls run with root user $N"  | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGFOLDER
+
+VALIDATE (){
+if [ $1 -ne 0 ]; then
+    echo -e "$2....$R FAILED $N" | tee -a $LOGS_FILE
+    exit 1
+else
+    echo -e "$2  ....$G SUCCESS $N" | tee -a $LOGS_FILE
+fi
+
+}
+
+
+dnf install maven -y &>>$LOGS_FILE
+VALIDATE $? "Install MAVEN"
+
+#creating system user
+id roboshop &>>$LOGS_FILE
+
+if [ $? -ne 0 ]; then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+        VALIDATE $? "create system user"
+ else
+        echo -e "Roboshop USER Already Existing.... $Y SKIPPING $N "
+fi
+
+mkdir  -p /app  
+VALIDATE $? "create directory app"
+
+curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOGS_FILE
+VALIDATE $? "Download shipping code"
+
+cd /app 
+VALIDATE $? "Move to directory app"
+
+rm -rf /app/*
+VALIDATE $? "Removing Existing Code"
+
+unzip /tmp/shipping.zip &>>LOGFILENAME
+VALIDATE $? "Unzip shipping"
+
+
+cd /app 
+VALIDATE $? "Move to directory app"
+
+mvn clean package &>>LOGFILENAME
+mv target/shipping-1.0.jar shipping.jar 
+VALIDATE $? "Move shipping to target"
+
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
+VALIDATE $? "Created systemctl service"
+
+dnf install mysql -y  &>>$LOGS_FILE
+VALIDATE $? "Install MYSQL"
+
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
+if [ $? -ne 0 ]; then
+
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
+    VALIDATE $? "Loaded data into MySQL"
+else
+    echo -e "data is already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl enable shipping &>>$LOGS_FILE
+systemctl start shipping
+VALIDATE $? "Enabled and started shipping"
